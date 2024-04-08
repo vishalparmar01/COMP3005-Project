ab2d7dd20391591e7d279b93c0c24222ac038577
tanayshah20 <tanayshah3@cmail.carleton.ca>
2024-04-08 19:49:01 -0400

    Added CSS to HTML pages

--- gymmanagement/gymmanagement/settings.py
+++ gymmanagement/gymmanagement/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'healthproject',
         'USER': 'postgres',
-        'PASSWORD': 'vishal.24',
+        'PASSWORD': 'tanayShah',
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
